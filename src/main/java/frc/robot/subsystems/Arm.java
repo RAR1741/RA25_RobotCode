edc4fb9b4d8ba04f2ca6d7bac98b2d5b76bdb9d2
NotMePipe <mepipeofficial@gmail.com>
2025-01-18 14:04:50 -0500

    Create Arm class

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems;
+
+public class Arm extends Subsystem{
+
+    private Arm() {
+        super("Arm");
+    }
+
+    @Override
+    public void reset() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'reset'");
+    }
+
+    @Override
+    public void periodic() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'periodic'");
+    }
+
+    @Override
+    public void writePeriodicOutputs() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writePeriodicOutputs'");
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stop'");
+    }
+
+}
