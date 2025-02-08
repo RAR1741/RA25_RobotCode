3f201daaff191cce71f4502fd70bee1a7c3e8557
dracco1993 <dracco1993@gmail.com>
2025-02-08 16:40:56 -0500

    Reenable things in Robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 
 import org.littletonrobotics.junction.LoggedRobot;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.constants.RobotConstants;
@@ -16,8 +17,13 @@ import frc.robot.controls.controllers.OperatorController;
 import frc.robot.controls.controllers.VirtualRobotController;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Arm.ArmState;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Elevator.ElevatorState;
+import frc.robot.subsystems.PoseAligner;
 import frc.robot.subsystems.SignalManager;
 import frc.robot.subsystems.Subsystem;
+import frc.robot.subsystems.drivetrain.RAROdometry;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
 
 /**
  * The methods in this class are called automatically corresponding to each
@@ -29,12 +35,12 @@ import frc.robot.subsystems.Subsystem;
 public class Robot extends LoggedRobot {
   private final ArrayList<Subsystem> m_subsystems;
 
-  // private final SwerveDrive m_swerve;
-  // private final Elevator m_elevator;
+  private final SwerveDrive m_swerve;
+  private final Elevator m_elevator;
   private final Arm m_arm;
-  // private final EndEffector m_endAffector;
-  // private final RAROdometry m_odometry;
-  // private final PoseAligner m_poseAligner;
+  // private final EndEffector m_endEffector;
+  private final RAROdometry m_odometry;
+  private final PoseAligner m_poseAligner;
 
   private final DriverController m_driverController;
   private final OperatorController m_operatorController;
@@ -50,26 +56,23 @@ public class Robot extends LoggedRobot {
   public Robot() {
     RobotConstants.getInstance();
     m_subsystems = new ArrayList<>();
-    // m_swerve = SwerveDrive.getInstance();
-    // m_odometry = RAROdometry.getInstance();
+    m_swerve = SwerveDrive.getInstance();
+    m_odometry = RAROdometry.getInstance();
     m_arm = Arm.getInstance();
-    // m_elevator = Elevator.getInstance();
-    // m_endAffector = EndEffector.getInstance();
-    // m_poseAligner = PoseAligner.getInstance();
+    m_elevator = Elevator.getInstance();
+    // m_endEffector = EndEffector.getInstance();
+    m_poseAligner = PoseAligner.getInstance();
 
     m_driverController = new DriverController(0, true, true, 0.5);
     m_operatorController = new OperatorController(1, true, true, 0.5);
     m_virtualRobotController = new VirtualRobotController(2);
 
-    // SCARY
-    DriverStation.silenceJoystickConnectionWarning(true);
-
-    // m_subsystems.add(m_poseAligner);
-    // m_subsystems.add(m_swerve);
-    // m_subsystems.add(m_odometry);
+    m_subsystems.add(m_poseAligner);
+    m_subsystems.add(m_swerve);
+    m_subsystems.add(m_odometry);
     m_subsystems.add(m_arm);
 
-    // m_subsystems.add(m_endAffector);
+    // m_subsystems.add(m_endEffector);
 
     new RobotTelemetry();
 
@@ -121,44 +124,43 @@ public class Robot extends LoggedRobot {
     ySpeed *= slowScaler * boostScaler;
     rot *= slowScaler * boostScaler;
 
-    // Pose2d targetPose =
-    // m_poseAligner.getAndCalculateTargetPose(m_virtualRobotController.getCurrentPose());
-    // ASPoseHelper.addPose("VirtualRobot/target", targetPose);
+    Pose2d targetPose = m_poseAligner.getAndCalculateTargetPose(m_virtualRobotController.getCurrentPose());
+    ASPoseHelper.addPose("VirtualRobot/target", targetPose);
 
-    // Pose2d currentPose = m_odometry.getPose();
-    // Pose2d desiredPose = m_poseAligner.getAndCalculateTargetPose(currentPose);
-    // ASPoseHelper.addPose("VirtualRobot/target", desiredPose);
+    Pose2d currentPose = m_odometry.getPose();
+    Pose2d desiredPose = m_poseAligner.getAndCalculateTargetPose(currentPose);
+    ASPoseHelper.addPose("VirtualRobot/target", desiredPose);
 
-    // if (m_driverController.getWantsAutoPosition()) {
-    // m_swerve.drive(xSpeed, ySpeed, rot, true, currentPose, desiredPose);
-    // } else {
-    // m_swerve.drive(xSpeed, ySpeed, rot, true);
-    // }
+    if (m_driverController.getWantsAutoPosition()) {
+      m_swerve.drive(xSpeed, ySpeed, rot, true, currentPose, desiredPose);
+    } else {
+      m_swerve.drive(xSpeed, ySpeed, rot, true);
+    }
 
-    // if (m_driverController.getWantsResetOdometry()) {
-    // m_odometry.reset();
-    // }
+    if (m_driverController.getWantsResetOdometry()) {
+      m_odometry.reset();
+    }
 
-    // if (m_operatorController.getWantsGoToStow()) {
-    // m_elevator.goToElevatorPosition(ElevatorState.STOW);
-    // } else if (m_operatorController.getWantsGoToL1()) {
-    // m_elevator.goToElevatorPosition(ElevatorState.L1);
-    // } else if (m_operatorController.getWantsGoToL2()) {
-    // m_elevator.goToElevatorPosition(ElevatorState.L2);
-    // } else if (m_operatorController.getWantsGoToL3()) {
-    // m_elevator.goToElevatorPosition(ElevatorState.L3);
-    // } else if (m_operatorController.getWantsGoToL4()) {
-    // m_elevator.goToElevatorPosition(ElevatorState.L4);
-    // } else if (m_operatorController.getWantsResetElevator()) {
-    // m_elevator.reset();
-    // }
+    if (m_operatorController.getWantsGoToStow()) {
+      m_elevator.setState(ElevatorState.STOW);
+    } else if (m_operatorController.getWantsGoToL1()) {
+      m_elevator.setState(ElevatorState.L1);
+    } else if (m_operatorController.getWantsGoToL2()) {
+      m_elevator.setState(ElevatorState.L2);
+    } else if (m_operatorController.getWantsGoToL3()) {
+      m_elevator.setState(ElevatorState.L3);
+    } else if (m_operatorController.getWantsGoToL4()) {
+      m_elevator.setState(ElevatorState.L4);
+    } else if (m_operatorController.getWantsResetElevator()) {
+      m_elevator.reset();
+    }
 
     // if (m_operatorController.getWantsScore() > 0) {
-    // m_endAffector.setState(EndEffectorState.SCORE_BRANCHES);
+    // m_endEffector.setState(EndEffectorState.SCORE_BRANCHES);
     // } else if (m_operatorController.getWantsScore() < 0) {
-    // m_endAffector.setState(EndEffectorState.SCORE_TROUGH);
+    // m_endEffector.setState(EndEffectorState.SCORE_TROUGH);
     // } else {
-    // m_endAffector.setState(EndEffectorState.OFF);
+    // m_endEffector.setState(EndEffectorState.OFF);
     // if (m_driverController.getWantsAutoPositionPressed()) {
     // m_swerve.resetDriveController();
     // }
@@ -176,17 +178,14 @@ public class Robot extends LoggedRobot {
 
   @Override
   public void disabledPeriodic() {
-    // m_odometry.setAllianceGyroAngleAdjustment();
+    m_odometry.setAllianceGyroAngleAdjustment();
 
-    // if (m_operatorController.getWantsResetElevator()) {
-    // m_elevator.reset();
-    // }
-
-    if (m_operatorController.getWantsArmScore()) {
-      m_arm.setArmState(ArmState.SCORE);
-    } else if (m_operatorController.getWantsArmStow()) {
-      m_arm.setArmState(ArmState.STOW);
+    if (m_operatorController.getWantsResetElevator()) {
+      m_elevator.reset();
     }
+
+    // SCARY
+    DriverStation.silenceJoystickConnectionWarning(DriverStation.getMatchType() == DriverStation.MatchType.None);
   }
 
   @Override
