75088e7027a37e2e0f2eb058d88964b3c58a0ab7
rar1741programmer <programmer@redalert1741.org>
2025-03-21 19:58:50 -0400

    Lower right intake ground position

--- src/main/java/frc/robot/constants/MonarchConstants.java
+++ src/main/java/frc/robot/constants/MonarchConstants.java
@@ -38,7 +38,7 @@ public class MonarchConstants extends Constants {
     Intake.Left.k_stuckPosition = 0.5542285442352295;
 
     Intake.Right.k_stowPosition = 0.4848584532737732;
-    Intake.Right.k_groundPosition = 0.8430086374282837;
+    Intake.Right.k_groundPosition = 0.850831151008606; //0.8430086374282837;
     Intake.Right.k_ejectPosition = 0.7199869155883789;
     Intake.Right.k_horizontalPosition = 0.7199869155883789;
     Intake.Right.k_stuckPosition = 0.7980979681015015;
